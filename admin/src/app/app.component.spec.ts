29b42530ff0d224776c0bb01b67435868227b614
zerobug110 <mahabojang110@gmail.com>
2022-12-07 11:10:32 +0000

    initializing angular app

--- admin/src/app/app.component.spec.ts
+++ admin/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'xtramedic'`, () => {
+  it(`should have as title 'admin'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('xtramedic');
+    expect(app.title).toEqual('admin');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('xtramedic app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('admin app is running!');
   });
 });
--- admin/src/app/app.module.ts
+++ admin/src/app/app.module.ts
@@ -3,12 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HeaderComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
