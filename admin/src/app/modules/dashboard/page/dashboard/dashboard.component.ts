02fb975a07d57943a3514c0b32ba7f891d7db70c
zerobug110 <mahabojang110@gmail.com>
2023-01-14 03:07:54 +0000

    modify the graph

--- admin/src/app/modules/dashboard/page/dashboard/dashboard.component.ts
+++ admin/src/app/modules/dashboard/page/dashboard/dashboard.component.ts
@@ -20,36 +20,24 @@ export class DashboardComponent implements OnInit {
   ColumnMode = ColumnMode;
 
   //LIne chart
-    public lineChartData: ChartConfiguration<'line'>['data'] = {
-      labels: [
-        'January',
-        'February',
-        'March',
-        'April',
-        'May',
-        'June',
-        'July'
-      ],
-      datasets: [
-        {
-          data: [ 65, 59, 80, 81, 56, 55, 40 ],
-          label: 'Series A',
-          fill: true,
-          tension: 0.5,
-          borderColor: 'black',
-          backgroundColor: 'rgba(255,0,0,0.3)'
-        }
-      ]
-    };
-    public lineChartOptions: ChartOptions<'line'> = {
-      responsive: false
-    };
-    public lineChartLegend = true;
+  public barChartLegend = true;
+  public barChartPlugins = [];
+
+  public barChartData: ChartConfiguration<'bar'>['data'] = {
+    labels: [ '2006', '2007', '2008', '2009', '2010', '2011', '2012' ],
+    datasets: [
+      { data: [ 65, 59, 80, 81, 56, 55, 40 ], label: 'Series A' },
+      { data: [ 28, 48, 40, 19, 86, 27, 90 ], label: 'Series B' }
+    ]
+  };
+
+  public barChartOptions: ChartConfiguration<'bar'>['options'] = {
+    responsive: false,
+  };
 
 
-    //pie chat
 
-  // Pie
+  //pie chat
   public pieChartOptions: ChartOptions<'pie'> = {
     responsive: false,
   };
