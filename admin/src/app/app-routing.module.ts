805549607be57d05b34a261b316ed21d1891e833
zerobug110 <mahabojang110@gmail.com>
2023-01-25 02:29:54 +0000

    enhance dashboard app ui

--- admin/src/app/app-routing.module.ts
+++ admin/src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './modules/dashboard/page/dashboard/dashboard.component';
 
 const routes: Routes = [
-  {path: 'dashboard', component: DashboardComponent}
+  {path: "", component: DashboardComponent}
 ];
 
 @NgModule({
